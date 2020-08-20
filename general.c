34a39b388f9795c91165e8095c692da7cea5976e
Abolfazl Rostamzadeh <a.rostamzadeh@gmail.com>
2020-08-21 07:47:07 +1200

    add time to debug_msg()

--- general.c
+++ general.c
@@ -3,7 +3,38 @@
 #define TIMER_CLOCK_FREQUENCY_HZ	170000000
 
 #ifdef DEBUG_MODE
-uint8_t tX_buff[TX_BUF_COL][TX_BUF_ROW], tx_buff_counter = 0, usart_has_initialized = 0;
+uint8_t tX_buff[TX_BUF_COL][TX_BUF_ROW], tx_buff_counter = 0;
+#endif
+
+#ifdef DEBUG_MODE
+void debug_msg(uint8_t debug, uint8_t time, uint32_t counter,
+		char const *format, ...) {
+	static uint32_t debug_msg_counter = 0;
+	if (counter > 0) {
+		debug_msg_counter++;
+		if (debug_msg_counter > counter)
+			return;
+	}
+	va_list args;
+	if (debug) {
+		int space = 0;
+		if (time) {
+			space = 5;
+			sprintf(((char*) tX_buff[tx_buff_counter]), "%lu)   ",
+					(uint32_t) (HAL_GetTick() / 1000));
+		}
+		va_start(args, format);
+		if (strlen(format) < TX_BUF_ROW)
+			vsprintf(((char*) tX_buff[tx_buff_counter]) + space, format, args);
+		else
+			vsprintf(((char*) tX_buff[tx_buff_counter]) + space,
+					">=TX_BUF_ROW strlen error", args);
+		rotateVal(&tx_buff_counter, 0, TX_BUF_COL - 1);
+		va_end(args);
+	}
+}
+#else
+#define debug_msg(...)
 #endif
 
 void timerSetFreq(TIM_HandleTypeDef *htim, uint32_t frq_Hz) {
