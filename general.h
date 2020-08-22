034173ff2eb393d1d43c238f59fbf119eee09472
Abolfazl Rostamzadeh <a.rostamzadeh@gmail.com>
2020-08-23 00:24:42 +1200

    only general functions

--- general.h
+++ general.h
@@ -17,6 +17,7 @@ extern "C" {
 #include <string.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #ifdef DEBUG_MODE
 #define  TX_BUF_COL     50  //max number of messages
@@ -26,21 +27,41 @@ extern "C" {
 #endif
 #if (TX_BUF_ROW < 1 || TX_BUF_ROW > 254)
 #error "TX_BUF_ROW is out of range!"
-#endif	
+#endif
 extern uint8_t tX_buff[TX_BUF_COL][TX_BUF_ROW], tx_buff_counter;
 #endif
 
 #ifdef DEBUG_MODE
 void debug_msg(uint8_t debug, uint8_t time, uint32_t counter,
 		char const *format, ...) ;
-#endif	
-	
-void timerSetFreq(TIM_HandleTypeDef *htim, uint32_t frq_Hz);
+#endif
 
-static inline void blinkLED(void) {
-	static uint8_t one_hz = 0;
-	if (++one_hz >= 10) { // 1 Hz
-		one_hz = 0;
+void testButton(GPIO_TypeDef *port, uint16_t pin, uint8_t *glitch_protection,
+		uint8_t *pressed, uint8_t *last_pressed, uint8_t *just_pressed);
+
+#define isInRaisingEdge(port, pin) 			readDigitalPin(port, pin)
+#define isInFallingEdge(port, pin) 			(!readDigitalPin(port, pin))
+#define disablePin(port, pin)				setPinAsInput(port, pin)
+#define setPinHighImpedance(port, pin)		setPinAsInput(port, pin)
+#define setPinHigh(port, pin)				HAL_GPIO_WritePin(port, pin, GPIO_PIN_SET);
+#define setPinLow(port, pin)				HAL_GPIO_WritePin(port, pin, GPIO_PIN_RESET);
+
+static inline uint8_t readDigitalPin(GPIO_TypeDef *port, uint16_t pin)
+{
+  if( HAL_GPIO_ReadPin(port, pin)==GPIO_PIN_SET )
+	  return 1;
+  return 0;
+}
+
+static inline void blinkLedInTimer(uint32_t timer_frq_hz,
+		uint32_t blink_frq_hz) {
+	static uint32_t frq_hz = 0, cnt;
+	if (frq_hz == 0){
+		if(blink_frq_hz==0) return;
+		cnt = (timer_frq_hz / blink_frq_hz) >> 1;
+	}
+	if (++frq_hz >= cnt) {
+		frq_hz = 0;
 		HAL_GPIO_TogglePin(StatusLED_GPIO_Port, StatusLED_Pin);
 	}
 }
@@ -52,7 +73,7 @@ static inline void rotateVal(uint8_t *val, uint8_t min, uint8_t max) {
 		*val = min;
 }
 #endif
-	
+
 #ifdef DEBUG_MODE
 static inline void sendTxBuffer(void) {
 	static unsigned char last_tx_buff_counter = 0;
@@ -64,6 +85,35 @@ static inline void sendTxBuffer(void) {
 	}
 }
 #endif
+
+static inline void setPinAsInput(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin) {
+	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+	GPIO_InitStruct.Pin = GPIO_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_DeInit(GPIOx, GPIO_Pin);
+	HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);
+}
+
+static inline void setPinAsOutput(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin,
+		GPIO_PinState PinState) {
+	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+	HAL_GPIO_WritePin(GPIOx, GPIO_Pin, PinState);
+	GPIO_InitStruct.Pin = GPIO_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);
+}
+
+static inline uint8_t isTimeElapsed(uint32_t *ms, uint32_t update_rate) {
+	uint32_t now = HAL_GetTick();
+	if ((now - *ms > update_rate) || (*ms > now)) {
+		*ms = now;
+		return 1;
+	}
+	return 0;
+}
 
 #ifdef __cplusplus
 }
